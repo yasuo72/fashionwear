aad189d5aa269dbbdb97fba04b5b8fe1830874c9
Your Name <kingrohi37@gmail.com>
2025-10-14 20:41:43 +0530

    Fix: Listen on 0.0.0.0 for Railway

--- server/index.ts
+++ server/index.ts
@@ -77,10 +77,12 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
+  const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+  
   server.listen({
     port,
-    host: "localhost",
+    host,
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on ${host}:${port}`);
   });
 })();
